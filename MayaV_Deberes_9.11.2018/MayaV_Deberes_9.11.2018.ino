c203327cb40eacaa4e562fc835ecc9ebc13959c7
ChenteMaya <44549797+ChenteMaya@users.noreply.github.com>
2018-11-09 08:33:45 -0500

    Update MayaV_Deberes_9.11.2018/MayaV_Deberes_9.11.2018.ino

--- MayaV_Deberes_9.11.2018/MayaV_Deberes_9.11.2018.ino
+++ MayaV_Deberes_9.11.2018/MayaV_Deberes_9.11.2018.ino
@@ -1,72 +1,37 @@
-#include <ArduinoSort.h>
-
 #include"datos_entrenamiento.h"
 #include"datos_test.h"
+
+
 int col=0;
 int row=0;
 float distancia;
-float distancia_menor=1000;
+float dist_menor=1000;
+int tcont=0;
 
-//variables posobles
-int test=80;
-byte valores[120];
-int tipos[3];
-int tipo_flor=0;
 int d1=0;
 int d2=0;
 int d3=0;
-int tiempo=0;
+int valor_p=20;// se puede poner cualquier valor de datos_test para hacer pruebas
 
 void setup() {
-  
-  Serial.begin(9600);
-
-//  for(;row<120;row++){
-//    for(;col<5;col++){
-//      Serial.print(matriz[row][col]);
-//      Serial.print(',');
-//      
-//      }
-//      col=0;
-//      Serial.println(' ');
-//    }
-
- }
-
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+}
 
 void loop() {
 
-//el while para que se ejecute una sola vez, sin necesidad de interrupcion
-while(tiempo==0){
-for(;row<120;row++){
-   
-      distancia=sqrt( pow((matriz[row][0]-prueba[test][0]),2)+pow((matriz[row][1]-prueba[test][1]),2)+ pow((matriz[row][2]-prueba[test][2]),2)+pow((matriz[row][3]-prueba[test][3]),2)+pow((matriz[row][4]-prueba[test][4]),2));
-        
-
-valores[row]=distancia;
-      
-        Serial.println("");
-        Serial.print("int: ");
-        Serial.print(distancia);
-         Serial.print("byte: ");
-        Serial.print( valores[row]);
-
-    }
-
-   for(;col<120;col++){
- 
-  Serial.println(valores[col]);
-}
-     
-
-tiempo=1;
-  }
-}
-
-//selecciona el tipo de flor q es para mostrar en serial
-
-void tipo(){
-   switch(tipo_flor){
+  for(;row<120;row++){ // contador de filas (for lento)
+       distancia= sqrt( pow((matriz[row][0]-prueba[valor_p][0]),2)+
+                        pow((matriz[row][1]-prueba[valor_p][1]),2)+
+                        pow((matriz[row][2]-prueba[valor_p][2]),2)+
+                        pow((matriz[row][3]-prueba[valor_p][3]),2));
+      if(distancia<dist_menor){ 
+       dist_menor=distancia;   
+       Serial.println(dist_menor);
+       tcont=matriz[row][4];
+       Serial.println(tcont);
+       
+         switch(tcont){
           case 1:
                  Serial.println("Setosa");
                  d1++;
@@ -79,5 +44,23 @@ void tipo(){
                 Serial.println("Virginica");
                 d3++;
                  break;
+        
         }
+       
+     }             
+      
+  }
+
+/*
+ * Lectura de datos
+  for(;row<120;row++){ // contador de filas (for lento)
+    for(;col<5;col++){ // contador de col (for rapido)
+      Serial.print(matriz[row][col]);  // imprime dato de matriz
+      Serial.print(',');            // separador de dato
+      delay(25);
+      }
+      col=0;        // reinicio de columna
+      Serial.println(' ');   // caracter en blanco de avance de linea
+  }
+  */    
 }
